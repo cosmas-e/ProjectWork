6544cd2814da9bc87eec23e94423903b770b266a
Emediong Cosmas <emediongcosmas1@gmail.com>
2018-03-23 03:40:53 +0000

    Updated

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { HomePage } from '../home/home';
 
 /**
  * Generated class for the RegisterPage page.
@@ -20,29 +19,22 @@ export class RegisterPage {
   @ViewChild('username') user;
   @ViewChild('password') password;
 
-  constructor(private alertCtrl: AlertController, private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
-  alert(message: string) {
-    this.alertCtrl.create({
-      title: 'Info!',
-      subTitle: message,
-      buttons: ['OK']
-    }). present();
-    }
 
+  
 registerUser() {
   this.fire.auth.createUserWithEmailAndPassword(this.user.value, this.password.value)
   .then(data =>{
    console.log('got data', data);
 
-   this.alert("Sucess! You are registered!");
-   this.navCtrl.setRoot( HomePage );
   })
+
   .catch(error =>{
     console.log('got an error', error);
   });
